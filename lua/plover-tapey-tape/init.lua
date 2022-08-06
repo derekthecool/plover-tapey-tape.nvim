3eba6314df613beed9357b09e04e272c3bc5a3db
derekthecool <derekthecool@gmail.com>
2022-08-06 00:00:52 -0600

    Changes to read last line only, but still seeing issues with messed up status line

--- lua/plover-tapey-tape/init.lua
+++ lua/plover-tapey-tape/init.lua
@@ -1,15 +1,20 @@
 -- get all lines from a file, returns an empty
 -- list/table if the file does not exist
-local function lines_from(file)
-  local lines = {}
-  for line in io.lines(file) do
-    lines[#lines + 1] = line
-  end
-  return lines
-end
+-- local function lines_from(file)
+--   local lines = {}
+--   for line in io.lines(file) do
+--     lines[#lines + 1] = line
+--   end
+--   return lines
+-- end
 
 -- local f = io.open('/mnt/c/Users/Derek Lomax/AppData/Local/plover/plover/tapey_tape.txt', 'r')
-local lines = lines_from('/mnt/c/Users/Derek Lomax/AppData/Local/plover/plover/tapey_tape.txt')
+-- local lines = lines_from('/mnt/c/Users/Derek Lomax/AppData/Local/plover/plover/tapey_tape.txt')
+local f = io.open('/mnt/c/Users/Derek Lomax/AppData/Local/plover/plover/tapey_tape.txt')
+local len = f:seek('end')
+f:seek('set', len - 40)
+local last_line = f:read('l')
+f:close()
 
-TapeyTape = lines[#lines]
+TapeyTape = last_line
 vim.inspect('In plugin plover-tapey-tape, setting the global variable TapeyTape to: ' .. TapeyTape)
