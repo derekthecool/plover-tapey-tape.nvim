37fd4ab47bd7c4646f44264597c35bf5894aea29
derekthecool <derekthecool@gmail.com>
2022-10-03 11:19:03 -0600

    Avoiding vim file watcher setup, use basic logic instead

--- lua/plover-tapey-tape/init.lua
+++ lua/plover-tapey-tape/init.lua
@@ -8,112 +8,121 @@ local tapey_tape_file = utils.get_tapey_tape_filename()
 -- Add way to make file updates not show that the file is being written
 
 local function update()
-  -- Using normal file read
-  local tapey_tape_filename_in_update = tapey_tape_file
-  if not tapey_tape_filename_in_update then
-    return
-  end
-  local tapey_tape_file_in_update = io.open(tapey_tape_filename_in_update)
-
-  -- Go to end of the file and then backwards by an offset
-  tapey_tape_file_in_update:seek('end', -200)
-  local line = ''
-  for _ = 1, 10 do
-    local current_line = tapey_tape_file_in_update:read('l')
-    if current_line then
-      current_line = current_line:match('(|.*|)')
-      line = current_line
-    else
-      break
+    -- Using normal file read
+    local tapey_tape_filename_in_update = tapey_tape_file
+    if not tapey_tape_filename_in_update then
+        return
     end
-  end
-  if tapey_tape_file_in_update then
-    tapey_tape_file_in_update:close()
-  end
+    local tapey_tape_file_in_update = io.open(tapey_tape_filename_in_update)
 
-  TapeyTape = line
+    -- Go to end of the file and then backwards by an offset
+    tapey_tape_file_in_update:seek('end', -200)
+    local line = ''
+    for _ = 1, 10 do
+        local current_line = tapey_tape_file_in_update:read('l')
+        if current_line then
+            current_line = current_line:match('(|.*|%[.*%])')
+            line = current_line
+        else
+            break
+        end
+    end
+    if tapey_tape_file_in_update then
+        tapey_tape_file_in_update:close()
+    end
+
+    if line ~= previous_line then
+        previous_line = line
+        -- if TapeyTape ~= newTapeyTapeLine then
+        local newTapeyTapeLine = (line:gsub('%%', '%%%%')):match('(|.*|)')
+        TapeyTape = newTapeyTapeLine
+
+        if tapey_tape_buffer_number ~= nil then
+            vim.api.nvim_buf_set_lines(tapey_tape_buffer_number, -1, -1, false, { TapeyTape })
+            -- vim.api.nvim_win_set_cursor(
+            --     tapey_tape_window_number,
+            --     { vim.api.nvim_buf_line_count(tapey_tape_buffer_number), 0 }
+            -- )
+        end
+    end
 end
 
 local function open_tapey_tape(open_method)
-  local opts = require('plover-tapey-tape.opts')
+    local opts = require('plover-tapey-tape.opts')
 
-  if not open_method then
-    open_method = 'split'
-  end
+    if not open_method then
+        open_method = 'split'
+    end
 
-  local current_window = vim.api.nvim_get_current_win()
+    local buffer_name = 'TapeyTape'
 
-  local tapey_tape_file_in_open_tapey_tape = io.open(tapey_tape_file)
-  if not tapey_tape_file_in_open_tapey_tape then
-    return
-  end
+    local buffer_exists = false
+    for key, value in pairs(vim.fn.getbufinfo()) do
+        if value == buffer_name then
+            buffer_exists = true
+        end
+    end
 
-  vim.cmd(open_method .. ' ' .. tapey_tape_file)
-  tapey_tape_buffer_number = vim.api.nvim_win_get_buf(0)
-  tapey_tape_window_number = vim.api.nvim_get_current_win()
+    if not buffer_exists then
+        tapey_tape_buffer_number = vim.api.nvim_create_buf(true, true)
+        vim.api.nvim_buf_set_name(tapey_tape_buffer_number, buffer_name)
+    end
 
-  -- Save current CWD so auto root plugins don't mess it up
+    if open_method == 'split' then
+        vim.cmd([[sbuffer ]] .. buffer_name)
+        vim.api.nvim_win_set_height(tapey_tape_window_number, opts.vertical_split_height)
+    elseif open_method == 'vsplit' then
+        vim.cmd([[vertical sbuffer ]] .. buffer_name)
+        local width = utils.detect_tapey_tape_line_width()
+        -- vim.api.nvim_win_set_width(tapey_tape_window_number, width)
+        -- V(width)
+    end
+
+    tapey_tape_window_number = vim.api.nvim_get_current_win()
+    --[[ -- Save current CWD so auto root plugins don't mess it up
   utils.block_auto_change_directory('start')
-
-  if open_method == 'split' then
-    vim.api.nvim_win_set_height(tapey_tape_window_number, opts.vertical_split_height)
-  elseif open_method == 'vsplit' then
-    vim.api.nvim_win_set_width(tapey_tape_window_number, utils.detect_tapey_tape_line_width())
-  end
 
   utils.block_auto_change_directory('stop')
 
   watch_tapey_tape_for_changes(tapey_tape_buffer_number, tapey_tape_file)
-  vim.api.nvim_set_current_win(current_window)
-end
-
-local w = vim.loop.new_fs_poll()
-
-local function on_change(bufNr, filePath)
-  vim.api.nvim_command('checktime ' .. bufNr)
-  w:stop()
-  watch_tapey_tape_for_changes(bufNr, filePath)
-  -- Lua API method to jump to end of file
-  if tapey_tape_window_number ~= nil and tapey_tape_buffer_number ~= nil then
-    vim.api.nvim_win_set_cursor(
-      tapey_tape_window_number,
-      { vim.api.nvim_buf_line_count(tapey_tape_buffer_number), 0 }
-    )
-  end
-end
-
-function watch_tapey_tape_for_changes(bufNr, filePath)
-  w:start(
-    filePath,
-    1000,
-    vim.schedule_wrap(function(...)
-      on_change(bufNr, filePath)
-    end)
-  )
+  vim.api.nvim_set_current_win(current_window) ]]
 end
 
 local function start()
-  -- Starting timer in neovim : https://stackoverflow.com/questions/68598026/running-async-lua-function-in-neovim
-  local timer = vim.loop.new_timer()
-  timer:start(
-    0,
-    30,
-    vim.schedule_wrap(function()
-      if tapey_tape_window_number ~= nil then
-        vim.api.nvim_win_set_cursor(tapey_tape_window_number, { vim.api.nvim_buf_line_count(0), 0 })
-      end
-      update()
-    end)
-  )
+    -- Starting timer in neovim : https://stackoverflow.com/questions/68598026/running-async-lua-function-in-neovim
+    local timer = vim.loop.new_timer()
+    timer:start(
+        0,
+        90,
+        vim.schedule_wrap(function()
+            local function win_exists(win_number)
+                local found = false
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    if win == win_number then
+                        found = true
+                    end
+                end
+                return found
+            end
+
+            if tapey_tape_window_number ~= nil and win_exists(tapey_tape_window_number) then
+                vim.api.nvim_win_set_cursor(
+                    tapey_tape_window_number,
+                    { vim.api.nvim_buf_line_count(tapey_tape_buffer_number), 0 }
+                )
+            end
+            update()
+        end)
+    )
 end
 
 local function stop()
-  TapeyTape = nil
+    TapeyTape = nil
 end
 
 return {
-  start = start,
-  stop = stop,
-  open = open_tapey_tape,
-  setup = utils.setup,
+    start = start,
+    stop = stop,
+    open = open_tapey_tape,
+    setup = utils.setup,
 }
